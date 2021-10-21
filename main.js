906dedb020bfb9a4331a4cfeb34fb95afd235b59
welschoff <h.welschoff@gmx.de>
2021-10-21 10:31:47 +0200

    :coffin: Remove dead code
    Remove dead code

--- main.js
+++ main.js
@@ -21,25 +21,6 @@ async function renderApp() {
   const body = await response.json();
   const characters = body.results;
 
-  // const characters = [
-  //   {
-  //     imgSrc: 'https://rickandmortyapi.com/api/character/avatar/14.jpeg',
-  //     name: 'Alien Morty',
-  //     status: 'Alive',
-  //     race: 'Alien',
-  //     location: 'Citadel of Ricks',
-  //     firstSeen: 'Close Rick-counters of the Rick Kind',
-  //   },
-  //   {
-  //     imgSrc: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg',
-  //     name: 'Jerry Smith',
-  //     status: 'Alive',
-  //     race: 'human',
-  //     location: 'Earth (C-137)',
-  //     firstSeen: 'Pilot',
-  //   },
-  // ];
-
   const characterCards = characters.map((character) =>
     createCharacterCard(character)
   );
